9fe4c57dfffedd08cdb64a8bb329e9d4d6bd570b
dlind <dlind@liveops.com>
2014-04-03 22:40:40 -0400

    Use ruby magic rather than wget / curl to fetch stuff so we're more agnostic about where we run. Clean up the logging/error handling a little bit.

--- lib/winter/dependency.rb
+++ lib/winter/dependency.rb
@@ -15,6 +15,7 @@
 require 'winter/logger'
 require 'fileutils'
 require 'digest/md5'
+require 'net/http'
 
 module Winter
 
@@ -41,11 +42,19 @@ module Winter
     end
     
     def get    
-      FileUtils.mkdir_p @destination unless File.directory?(@destination) 
-      #Skip the pig that is mvn if we're going to the artifactory
+      if ! File.directory?(@destination)  
+        $LOG.debug "Create the destination folder if its not already there."
+        FileUtils.mkdir_p @destination  
+      end
+      #Try and get file in a couple different ways. If we succeed then move on. 
+      #Artifacts in your local .m2 ALWAYS take precedence over a remote artifact...
+      #Unless we fail so hard we end up using mvn as a last ditched effort. 
       success = getLocalM2
       success = getRestArtifactory if ! success and ! @offline 
-      success = getMaven if ! success and system( 'which mvn > /dev/null' ) #Falback to mvn... if we can :(
+      success = getMaven if ! success and system( 'which mvn > /dev/null' ) 
+      if ! success
+        $LOG.info "[failed] #{outputFilename}"
+      end
       return success
     end
     
@@ -56,43 +65,63 @@ module Winter
     def dest_file
       File.join(@destination,outputFilename)
     end
-
+    
+    def restRequest(uri)
+      response = Net::HTTP.get_response(uri)
+      $LOG.debug "#{outputFilename}: Rest request to #{uri.inspect} #{response.inspect}"
+      return response.body if response.is_a?(Net::HTTPSuccess)
+      raise "Rest request got a bad response code back [#{response.code}]" 
+    end
+    
     def getRestArtifactory 
-      $LOG.info "Try and fetch #{dest_file}."
-      $LOG.debug "Create the destination folder if its not already there."
-      success = false
+      $LOG.debug "#{outputFilename}: Attempting to fetch via the artifactory rest api."
+      #Loop through all the repos until something works
       @repositories.each { |repo| 
-        c =  "exec wget #{repo}/#{artifactory_path}"
-        c << " -O #{dest_file} &>/dev/null"
-        $LOG.info c 
-        
-        if system( c )
-          success = true
-          m = "curl #{repo}/#{artifactory_path}.md5 2>/dev/null"
-          $LOG.debug m
-          artifactory_md5 = `#{m}`
+        begin
+          open(dest_file,"wb") { |file|
+            file.write(restRequest(URI.parse("#{repo}/#{artifactory_path}")))
+          }
+        rescue RuntimeError => e # :( Maybe do better handling later. 
+          $LOG.error "#{outputFilename}: Unable to fetch Artifact from #{repo}/#{artifactory_path}"  
+          $LOG.debug e
+          next
+        end 
+        #Check to make sure the md5sum of what we downloaded matches what's in the artifactory.
+        begin
+          artifactory_md5 = restRequest(URI.parse("#{repo}/#{artifactory_path}.md5"))
           my_md5 = Digest::MD5.file("#{dest_file}").hexdigest
-          $LOG.info "Comparing remote MD5 #{artifactory_md5} to local md5 #{my_md5} (#{dest_file})" 
-          break if artifactory_md5 == my_md5
-          $LOG.info "Comparison failed. Deleting the jar and signalling a failure" 
-          success = false
+        rescue RuntimeError => e # :( Do better handling later. 
+          $LOG.error "#{outputFilename}: Blew up while attempting to get md5s."
+        else
+          if artifactory_md5 == my_md5
+            $LOG.debug "#{outputFilename}: Successfully fetched via artifactory rest api."
+            $LOG.info "[remote] #{outputFilename}"
+            return true 
+          end
+          $LOG.error "#{outputFilename}: Comparing remote MD5 #{artifactory_md5} to local md5 #{my_md5} (#{dest_file})" 
+          $LOG.error "#{outputFilename}: Comparison failed. Deleting the 'bad' jar and moving on." 
           FileUtils.rm(dest_file)
         end
       }
-      $LOG.info "Fetch Successful." if success 
-      return success
+      return false
     end
     
     def getLocalM2 
       begin
         artifact = "#{ENV["HOME"]}/.m2/repository/#{artifactory_path}"
-        $LOG.info "Copying #{artifact} to #{dest_file}"
+        $LOG.debug "#{outputFilename}: Copying #{artifact} to #{dest_file}"
         FileUtils.cp(artifact,dest_file)
-      rescue Exception=>e
-        $LOG.error e 
-        return false
+      rescue Errno::ENOENT
+      	$LOG.debug "#{outputFilename}: #{artifact} does not exist."
+      rescue SystemCallError => e
+        $LOG.error "#{outputFilename}: Failed to copy file from local m2 repository."
+        $LOG.error e
+      else #Yay we got it
+      	$LOG.debug "#{outputFilename}: Successfully copied from local m2 repository."
+        $LOG.info "[local]  #{outputFilename}"
+        return true
       end
-      return true
+      return false
     end
 
     #Depricated because its slow and expensive to use... leaving it here for now.
