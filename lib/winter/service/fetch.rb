d83cb90270947b6eccbf8bede8cc61bb4293c211
dlind <dlind@liveops.com>
2014-05-09 20:28:11 -0400

    Raise an error if fetch fails so that we properly bombout

--- lib/winter/service/fetch.rb
+++ lib/winter/service/fetch.rb
@@ -51,7 +51,7 @@ module Winter
       dep.transative    = true
       dep.destination   = File.join('.')
 
-      dep.get
+      dep.get or raise "Failed to fetch jar."
 
       extract_jar "#{artifact}-#{version}.jar"
     end
